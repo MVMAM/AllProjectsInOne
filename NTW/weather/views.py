e747d0cfc0dae21d695990e0d4008bc95963362c
MMM <ediconpts@gmail.com>
2023-10-31 20:42:31 +0500

    first logic to W from NTW. (NTW is news, tasks and weather)

--- NTW/weather/views.py
+++ NTW/weather/views.py
@@ -1,5 +1,20 @@
+import requests
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    apiId = '6949a2808723bdd6406ed4175fdea64d'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + apiId
+
+    city = 'London'
+    responce = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city' : city,
+        'temp' : responce["main"]["temp"],
+        'icon' : responce["weather"][0]["icon"]
+    }
+
+    context = { 'info' : city_info }
+
+    return render(request, 'weather/index.html', context)
