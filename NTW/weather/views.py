16114c705903b48077fa762b20e8eb58b3386652
MMM <ediconpts@gmail.com>
2023-10-31 21:44:56 +0500

    Last commit in this repo. It was the biggest project i have ever done! I didn't want to finish this project bye :(

--- NTW/weather/views.py
+++ NTW/weather/views.py
@@ -1,20 +1,37 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     apiId = '6949a2808723bdd6406ed4175fdea64d'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + apiId
 
-    city = 'London'
-    responce = requests.get(url.format(city)).json()
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
 
-    city_info = {
-        'city' : city,
-        'temp' : responce["main"]["temp"],
-        'icon' : responce["weather"][0]["icon"]
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        responce = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city' : city.name,
+            'temp' : responce["main"]["temp"],
+            'icon' : responce["weather"][0]["icon"]
+        }
+
+        all_cities.append(city_info)
+
+    context = {
+        'all_info' : all_cities,
+        'form' : form
     }
-
-    context = { 'info' : city_info }
 
     return render(request, 'weather/index.html', context)
